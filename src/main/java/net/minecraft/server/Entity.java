9c19bf3198b04c73102377af495a546e8a1ee917
GJ <gjmcferrin@gmail.com>
2014-01-26 02:03:50 -0800

    [Bleeding] Prevent lightning strikes from spamming EntityDamageEvent. Fixes BUKKIT-2186
    Because lightning checks to see if it's hitting a player every tick, the
    EntityDamageEvent was being called repeatedly until the lightning despawned.
    This change ensures that the lightning will only fire events for players that
    it has not already hit.

--- src/main/java/net/minecraft/server/Entity.java
+++ src/main/java/net/minecraft/server/Entity.java
@@ -1575,6 +1575,10 @@ public abstract class Entity {
 
     public void a(EntityLightning entitylightning) {
         // CraftBukkit start
+        if (entitylightning.hasDamaged.contains(this)) {
+            return;
+        }
+
         final org.bukkit.entity.Entity thisBukkitEntity = this.getBukkitEntity();
         final org.bukkit.entity.Entity stormBukkitEntity = entitylightning.getBukkitEntity();
         final PluginManager pluginManager = Bukkit.getPluginManager();
@@ -1593,6 +1597,7 @@ public abstract class Entity {
             return;
         }
 
+        entitylightning.hasDamaged.add(this);
         this.burn((float) event.getDamage());
         // CraftBukkit end
 
