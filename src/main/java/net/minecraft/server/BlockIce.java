e88fc65e7c3076f4f5a56169294fa2cea0bea9d4
GJ <gjmcferrin@gmail.com>
2014-02-06 08:34:07 -0500

    [Bleeding] Call BlockFadeEvent with proper type in the nether. Fixes BUKKIT-5373
    Previously, whenever BlockFadeEvent was called for ice melting, it returned
    the new BlockState with a type of Material.STATIONARY_WATER. However, in the
    Nether, ice melting does not form water, but is simply replaced by air
    instead. This changes the event to use the proper BlockState based on whether
    the block is located in the Nether or not.

--- src/main/java/net/minecraft/server/BlockIce.java
+++ src/main/java/net/minecraft/server/BlockIce.java
@@ -44,11 +44,10 @@ public class BlockIce extends BlockHalfTransparent {
     public void a(World world, int i, int j, int k, Random random) {
         if (world.b(EnumSkyBlock.BLOCK, i, j, k) > 11 - this.k()) {
             // CraftBukkit start
-            if (org.bukkit.craftbukkit.event.CraftEventFactory.callBlockFadeEvent(world.getWorld().getBlockAt(i, j, k), Blocks.STATIONARY_WATER).isCancelled()) {
+            if (org.bukkit.craftbukkit.event.CraftEventFactory.callBlockFadeEvent(world.getWorld().getBlockAt(i, j, k), world.worldProvider.f ? Blocks.AIR : Blocks.STATIONARY_WATER).isCancelled()) {
                 return;
             }
             // CraftBukkit end
-
             if (world.worldProvider.f) {
                 world.setAir(i, j, k);
                 return;
